55168d8ce439d69e452a1d52a53eef5e09a4324d
David <david@ciedigital.com>
2019-12-02 13:47:13 -0800

    Day 1

---
+++ day01/Day01/Fuel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day01
+{
+    public class Fuel
+    {
+        public static decimal CalculateRocketShipFuel(List<int> input)
+        {
+            var total = 0m;
+            foreach (var module in input)
+            {
+                var fuelRequired = CalculateFuelRecursively(module);
+                total += fuelRequired;
+            }
+
+            return total;
+        }
+        public static decimal CalculateFuelRecursively(int fuelWeight)
+        {
+            var fuelForFuel = CalculateFuelRequirement(fuelWeight);
+            if (fuelForFuel <= 0)
+                return 0;
+            else return fuelForFuel + CalculateFuelRecursively((int)fuelForFuel);
+        }
+
+        public static decimal CalculateFuelRequirement(int weight)
+        {
+            var fuelRequired = Math.Round((decimal)(weight / 3), MidpointRounding.ToZero) - 2;
+            return fuelRequired;
+        }
+    }
+}
---
+++ day01/Day01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = LoadInput("input.txt");
+            var result = Fuel.CalculateRocketShipFuel(input);
+        }
+
+        public static List<int> LoadInput(string fileName)
+        {
+            var input = new List<int>();
+            using (TextReader reader = File.OpenText(fileName))
+            {
+                string currentLine;
+                while ((currentLine = reader.ReadLine()) != null)
+                {
+                    var current = int.Parse(currentLine);
+                    input.Add(current);
+                }
+            }
+
+            return input;
+        }
+    }
+}
