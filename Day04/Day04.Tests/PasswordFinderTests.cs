75dc3f48703abf82d8463394683d1f8b8c1cebe0
David Robison <davidrobison122@hotmail.com>
2019-12-05 09:57:29 -0800

    Day 4

---
+++ Day04/Day04.Tests/PasswordFinderTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Day04.Tests
+{
+    [TestFixture]
+    public class PasswordFinderTests
+    {
+        [TestCase(112233, true)] // valid
+        [TestCase(223450, false)] // not increasing
+        [TestCase(123789, false)] // no repeating
+        [TestCase(123444, false)] // repeating group, no pair
+        [TestCase(111122, true)] // repeating group
+        public void Validate(int input, bool isValid)
+        {
+            var result = PasswordFinder.IsValid(input);
+            Assert.AreEqual(isValid, result);
+        }
+
+        [Test]
+        public void ValidRange()
+        {
+            int start = 264793;
+            int end = 803935;
+            var result = PasswordFinder.CheckRange(start, end);
+        }
+    }
+}
---
+++ Day04/Day04/PasswordFinder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day04
+{
+    public class PasswordFinder
+    {
+        public static int CheckRange(int start, int end)
+        {
+            var possiblePasswords = new List<int>();
+
+            for (int index = start; index <= end; index++)
+            {
+                if (IsValid(index))
+                    possiblePasswords.Add(index);
+            }
+
+            return possiblePasswords.Count;
+        }
+
+        public static bool IsValid(int input)
+        {
+            var inputString = input.ToString();
+            int lastDigit = Convert.ToInt32(inputString[0]);
+            bool hasConsectiveMatching = false;
+            int inRepeatingRun = 1;
+            for (var index = 1; index < inputString.Length; index++)
+            {
+                var currentDigit = Convert.ToInt32(inputString[index]);
+                if (currentDigit < lastDigit) return false;
+                if (currentDigit == lastDigit)
+                {
+                    inRepeatingRun++;
+                }
+                else if (currentDigit != lastDigit && inRepeatingRun == 2)
+                {
+                    hasConsectiveMatching = true;
+                    inRepeatingRun = 1;
+                }
+                else
+                {
+                    inRepeatingRun = 1;
+                }
+                lastDigit = currentDigit;
+            }
+
+            if (inRepeatingRun == 2) hasConsectiveMatching = true;
+
+            return hasConsectiveMatching;
+        }
+    }
+}
